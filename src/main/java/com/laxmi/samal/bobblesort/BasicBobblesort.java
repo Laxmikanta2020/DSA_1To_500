1fd02d593bf98b1de33f627f36e1d43e3e474b02
Laxmikanta <akash83388@gmail.com>
2025-10-03 12:28:40 +0530

    Added bobble sort

--- src/main/java/com/laxmi/samal/bobblesort/BasicBobblesort.java
+++ src/main/java/com/laxmi/samal/bobblesort/BasicBobblesort.java
@@ -1,14 +1,57 @@
 package com.laxmi.samal.bobblesort;
 
+import java.util.Arrays;
+
 public class BasicBobblesort {
 
 
     public static void main(String[] args) {
         //it basic meaning swap next element
-        int [] arr={7,1,9,3,0,5};
+        int[] arr = {7, 1, 9, 3, 0, 5};
 
+        //if want to swap arr[i] to arr[i+1] element if arr[i+1] is less then the element
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+        Arrays.stream(arr).forEach(System.out::print);//in java 8 convert to array to stream
+
+        //array few element are sorted but not all element are sort for all we need to run multiple time
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+        System.out.println("After adding the loop it is sorted ");
+        System.out.print(Arrays.toString(arr));
+        // write in better ways if array is sorted no need to swap
+        arr = new int[]{7, 1, 9, 3, 0, 5};
+        boolean swapped;
+        for (int j = 0; j < arr.length; j++) {
+            swapped = true;
+            for (int i = 0; i < arr.length - 1 - j; i++) { //why we put -j ,in simple Every single loop we gat one index is sort
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = false;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+        System.out.print(Arrays.toString(arr));
 
 
     }
 }
+//Laxmikanat 03-10-2025#Bengaluro/hoodi
